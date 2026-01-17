4750837f535b28dc5ce79b217e5fcf171ae9b368
rich <richienagle@gmail.com>
2026-01-17 12:25:23 -0800

    Initial Commit

---
+++ Github Test/Github_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_TestApp.swift
+//  Github Test
+//
+//  Created by Rich Nagle on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Github_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
